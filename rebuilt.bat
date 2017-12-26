ae03e0c4dce3a5cae16d1989c11972456abbfba2
Drwalin <garekmarek@wp.pl>
2017-12-26 12:37:29 +0100

    Rebuilt correction.

--- rebuilt.bat
+++ rebuilt.bat
@@ -5,4 +5,4 @@ g++ -c "src/mtd/NavMesh.cpp" -m32 -O3 -o "bin/NavMesh.o"
 
 g++ -c "src/func/Debug.cpp" -m32 -O3 -o "bin/Debug.o"
 
-g++ -c "src/lib/MZ/SourceCode/AABB.cpp" "src/lib/MZ/SourceCode/GetSthFromString.cpp" "src/lib/MZ/SourceCode/Math.cpp" "src/lib/MZ/SourceCode/Octree.cpp" "src/lib/MZ/SourceCode/Quadtree.cpp" "src/lib/MZ/SourceCode/Quat.cpp" "src/lib/MZ/SourceCode/StandardManagerC.cpp" "src/lib/MZ/SourceCodeTriangle.cpp" "src/lib/MZ/SourceCode/Vector.cpp" "src/lib/MZ/SourceCode/VMatrix.cpp" -m32 -O3 -o "bin/LibMZ.o"
+g++ -c "src/lib/MZ/SourceCode/AABB.cpp" "src/lib/MZ/SourceCode/GetSthFromString.cpp" "src/lib/MZ/SourceCode/Math.cpp" "src/lib/MZ/SourceCode/Octree.cpp" "src/lib/MZ/SourceCode/Quadtree.cpp" "src/lib/MZ/SourceCode/Quat.cpp" "src/lib/MZ/SourceCode/StandardManagerC.cpp" "src/lib/MZ/SourceCode/Triangle.cpp" "src/lib/MZ/SourceCode/Vector.cpp" "src/lib/MZ/SourceCode/VMatrix.cpp" -m32 -O3 -o "bin/LibMZ.o"
