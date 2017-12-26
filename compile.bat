fa1eecc713288a25cafb5eeff2cd654c336e80bb
Drwalin <garekmarek@wp.pl>
2017-12-26 11:29:12 +0100

    Compile commands (1). Marek Zalewski license corrected.

---
+++ compile.bat
@@ -0,0 +1,2 @@
+
+g++ -O3 -m32 -o "release/KDG.exe" "bin/Main.o" "bin/Collider.o" "bin/NavMesh.o" "bin/Debug.o" "bin/LibMZ.o" -std=c++11
---
+++ rebuilt.bat
@@ -0,0 +1,8 @@
+
+g++ -c "src/Main.cpp" -m32 -O3 -o "bin/Main.o"
+g++ -c "src/mtd/Collider.cpp" -m32 -O3 -o "bin/Collider.o"
+g++ -c "src/mtd/NavMesh.cpp" -m32 -O3 -o "bin/NavMesh.o"
+
+g++ -c "src/func/Debug.cpp" -m32 -O3 -o "bin/Debug.o"
+
+g++ -c "src/lib/MZ/SourceCode/AABB.cpp" "src/lib/MZ/SourceCode/GetSthFromString.cpp" "src/lib/MZ/SourceCode/Math.cpp" "src/lib/MZ/SourceCode/Octree.cpp" "src/lib/MZ/SourceCode/Quadtree.cpp" "src/lib/MZ/SourceCode/Quat.cpp" "src/lib/MZ/SourceCode/StandardManagerC.cpp" "src/lib/MZ/SourceCodeTriangle.cpp" "src/lib/MZ/SourceCode/Vector.cpp" "src/lib/MZ/SourceCode/VMatrix.cpp" -m32 -O3 -o "bin/LibMZ.o"
