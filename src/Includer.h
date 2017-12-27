4e20537f27d638292ced95e6fa8310004fe7d283
Drwalin <garekmarek@wp.pl>
2017-12-27 12:14:46 +0100

    Licensing (2).

--- src/Includer.h
+++ src/Includer.h
@@ -19,22 +19,30 @@
 #include <queue>
 #include <map>
 
-// Opengl:
+// Marek Zalewski's libraries:
+
+#include "lib/MZ/SourceHeader/GetSthFromString.h"
+#include "lib/MZ/SourceHeader/StandardManagerC.h"
+#include "lib/MZ/SourceHeader/Quadtree.h"
+#include "lib/MZ/SourceHeader/Octree.h"
+#include "lib/MZ/SourceHeader/Math.h"
+#include "lib/MZ/SourceHeader/Vector.h"
+#include "lib/MZ/SourceHeader/VMatrix.h"
+#include "lib/MZ/SourceHeader/Quat.h"
+#include "lib/MZ/SourceHeader/AABB.h"
+#include "lib/MZ/SourceHeader/Triangle.h"
+
+// Graphic libraries:
 
 
 
-// Other libraries:
+// Sound libraries:
 
-#include "lib/MZ/SourceCode/AABB.h"
-#include "lib/MZ/SourceCode/GetSthFromString.h"
-#include "lib/MZ/SourceCode/Math.h"
-#include "lib/MZ/SourceCode/Octree.h"
-#include "lib/MZ/SourceCode/Quadtree.h"
-#include "lib/MZ/SourceCode/Quat.h"
-#include "lib/MZ/SourceCode/StandardManagerC.h"
-#include "lib/MZ/SourceCode/Triangle.h"
-#include "lib/MZ/SourceCode/Vector.h"
-#include "lib/MZ/SourceCode/VMatrix.h"
+
+
+// Inputs libraries:
+
+
 
 // Game:
 
