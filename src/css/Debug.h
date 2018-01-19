5bf4ffb065f0c43c1256813c4ab4cb92c6ca5c9e
Drwalin <garekmarek@wp.pl>
2018-01-19 23:28:20 +0100

    Debug method defined.

--- src/css/Debug.h
+++ src/css/Debug.h
@@ -7,17 +7,18 @@
 #define DEBUG_H
 
 #include <cstdio>
+#include <fstream>
 
 class Debug
 {
 private:
 	
-	
+	static std::ifstream file;
 	
 public:
 	
-	
-	
+	inline void Send( const std::string& msg );
+	static InitFile( const std::string& fileName );
 };
 
 #endif
---
+++ src/mtd/Debug.cpp
@@ -0,0 +1,29 @@
+
+#ifndef DEBUG_CPP
+#define DEBUG_CPP
+
+#include "../css/Debug.h"
+
+inline void Debug::Send( const std::string& msg )
+{
+	if( file.good() )
+	{
+		printf( "\n Debug: \"%s\" ", msg.c_str() );
+	}
+	else
+	{
+		printf( "\n Error: Debug can not open log file: \"%s\" ", fileName.c_str() );
+	}
+}
+
+static Debug::InitFile( const std::string& fileName )
+{
+	file.open( fileName.c_str() );
+	if( !file.good() )
+	{
+		printf( "\n Error: Debug can not open log file: \"%s\" ", fileName.c_str() );
+	}
+}
+
+#endif
+
